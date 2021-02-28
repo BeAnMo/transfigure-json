e4ae574acaa044e6a90425bc2daa840dbcc7ea6b
BeAnMo <bamorin00@gmail.com>
2021-02-28 13:50:15 -0600

    configuring package

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,14 @@
-import { terser } from 'rollup-plugin-terser';
+import { terser } from "rollup-plugin-terser";
 
 export default {
-    input: 'src/index.js',
-    output: [
-        {
-            file: 'dist/transfigure.js',
-            format: 'cjs',
-            exports: 'auto'
-        },
-        {
-            file: 'dist/transfigure-json.min.js',
-            format: 'cjs',
-            plugins: [terser()],
-            exports: 'auto'
-        },
-        {
-            file: 'dist/transfigure-json.browser.js',
-            format: 'iife',
-            name: 'Transfigurator',
-        },
-        {
-            file: 'dist/transfigure-json.browser.min.js',
-            format: 'iife',
-            name: 'Transfigurator',
-            plugins: [terser()]
-        }
-    ]
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/index.js",
+      format: "umd",
+      name: "Transfigurator",
+      sourcemap: true,
+      plugins: [terser()],
+    },
+  ],
 };
